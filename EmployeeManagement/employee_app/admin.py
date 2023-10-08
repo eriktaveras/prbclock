4756198addaa0004bab2aaa98d1f9efe2ab4b3bd
erik <eriktaveras@gmail.com>
2023-10-08 17:15:34 -0400

    admin

--- EmployeeManagement/employee_app/admin.py
+++ EmployeeManagement/employee_app/admin.py
@@ -8,23 +8,28 @@ class TimeRecordInline(admin.TabularInline):
     readonly_fields = ['date', 'time_in', 'time_out', 'lunch_time']
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'position')
+    list_display = ('name', 'position')  # Removed 'id' for better readability
     search_fields = ('name', 'position')
     list_filter = ('position',)
     inlines = [TimeRecordInline]
+    ordering = ['name']  # Default ordering
 
     fieldsets = (
         (_('Personal Information'), {'fields': ('name', 'position')}),
     )
 
 class TimeRecordAdmin(admin.ModelAdmin):
-    list_display = ('id', 'employee', 'date', 'time_in', 'time_out', 'lunch_time')
+    list_display = ('employee', 'date', 'time_in', 'time_out', 'lunch_time')  # Removed 'id' for better readability
     search_fields = ('employee__name', 'date')
     list_filter = ('date', 'employee')
+    date_hierarchy = 'date'  # Makes it easier to navigate through dates
+    ordering = ['date', 'employee']  # Default ordering
 
     fieldsets = (
         (_('Time Record Information'), {'fields': ('employee', 'date', 'time_in', 'time_out', 'lunch_time')}),
     )
+
+    list_select_related = ('employee',)  # Optimize SQL queries
 
 admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(TimeRecord, TimeRecordAdmin)
