5fcdfe4142a08a23025d3d59f6f0f7cf87de62b7
Erik Taveras <eriktaveras@gmail.com>
2023-10-09 13:02:09 -0400

    vercerl

--- EmployeeManagement/EmployeeManagement/settings.py
+++ EmployeeManagement/EmployeeManagement/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'  # Cambia 'America/New_York' a tu zona horaria preferida
 
 USE_I18N = True
 
@@ -124,4 +124,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TIME_ZONE = 'America/New_York'  # Cambia 'America/New_York' a tu zona horaria preferida
